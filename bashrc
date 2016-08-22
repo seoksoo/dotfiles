6e3d093d9fd3fd669a6d71c17ef54b6149858de7
sshong <Seok-Soo Hong>
2016-08-22 13:10:59 +0900

    need to re-coloring

--- bashrc
+++ bashrc
@@ -1,3 +1,9 @@
 if [ -f default ]; then
     source default
 fi
+
+for file in ./{prompt,alias}; do
+    #[ -r "$file" ] && [ -f "$file"] && source "$file";
+    source "$file"
+done;
+unset file
--- bootstrap.sh
+++ bootstrap.sh
@@ -5,4 +5,16 @@ git pull origin master
 git submodule init
 git submodule update
 
+function linking() {
+    [[ -a ~/.$1 ]] && rm -rf ~/.$1
+    ln -s $(pwd)/$1 ~/.$1
+}
 
+for file in {bashrc,vimrc,vim}; do
+    linking $file;
+done;
+unset file;
+
+source ~/.bashrc
+
+unset linking 
